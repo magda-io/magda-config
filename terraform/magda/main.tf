cfa003c5a43b96172e91c1a66f54734babaf6686
Jacky Jiang <jacky.jiang@data61.csiro.au>
2019-12-02 15:59:29 +1100

    make helm force update

--- terraform/magda/main.tf
+++ terraform/magda/main.tf
@@ -95,11 +95,12 @@ resource "kubernetes_namespace" "magda_namespace" {
 resource "helm_release" "magda_helm_release" {
   name = "magda"
   # or repository = "../../helm" for local repo
-  repository = "https://charts.magda.io/"
-  chart      = "magda"
-  version    = var.magda_version
-  devel      = var.allow_dev_magda_version
-  timeout    = 1800
+  repository   = "https://charts.magda.io/"
+  chart        = "magda"
+  version      = var.magda_version
+  devel        = var.allow_dev_magda_version
+  timeout      = 1800
+  force_update = true
 
   namespace = "${var.namespace}"
 
