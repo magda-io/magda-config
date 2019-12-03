4f1a2a4132d8047769718fd49d4a3881d1b21f5d
Jacky Jiang <jacky.jiang@data61.csiro.au>
2019-12-03 11:03:59 +1100

    lock helm provider to 0.10.2 to avoid the bug of helm v2 after 2.15

--- terraform/magda/main.tf
+++ terraform/magda/main.tf
@@ -2,6 +2,11 @@ terraform {
   # The modules used in this example have been updated with 0.12 syntax, which means the example is no longer
   # compatible with any versions below 0.12.
   required_version = ">= 0.12"
+  required_providers {
+    helm       = "0.10.2"
+    kubernetes = "1.10.0"
+    random     = "2.2.1"
+  }
 }
 
 provider "google-beta" {
